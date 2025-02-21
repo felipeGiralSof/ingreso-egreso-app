1ad23f11a5267113a22d5e3661d2b4d0ea676782
Felipe Giraldo <felipe-giraldo@hotmail.es>
2025-02-21 17:47:27 -0500

    culminando las bases de la app

---
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  standalone: false,
+  templateUrl: './estadistica.component.html',
+  styleUrl: './estadistica.component.css'
+})
+export class EstadisticaComponent {
+
+}
